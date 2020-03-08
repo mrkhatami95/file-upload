c6df7b3f4a21388b89f52fc16b62eacace689694
mrkhatami <mr.khatami95@gmail.com>
2020-03-09 00:14:47 +0330

    first commit

---
+++ src/main/java/com/mapsa/UploadDetail.java
@@ -0,0 +1,36 @@
+package com.mapsa;
+
+import java.io.Serializable;
+
+public class UploadDetail implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private long fileSize;
+	private String fileName, uploadStatus;
+
+
+	public long getFileSize() {
+		return fileSize;
+	}
+
+	public void setFileSize(long fileSize) {
+		this.fileSize = fileSize;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getUploadStatus() {
+		return uploadStatus;
+	}
+
+	public void setUploadStatus(String uploadStatus) {
+		this.uploadStatus = uploadStatus;
+	}
+}
